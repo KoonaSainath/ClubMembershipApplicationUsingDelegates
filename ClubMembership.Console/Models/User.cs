382c0dc3f108ae0e779aaa6c9f31358717dbedca
= <=>
2024-02-17 14:27:05 +0530

    Created User model

---
+++ ClubMembership.Console/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembership.Console.Models
+{
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int UserId { get; set; }
+
+        public string EmailId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string AddressLine1 { get; set; }
+
+        public string AddressLine2 { get; set;}
+
+        public string City { get; set; }
+
+        public string PostalCode { get; set; }
+    }
+}
