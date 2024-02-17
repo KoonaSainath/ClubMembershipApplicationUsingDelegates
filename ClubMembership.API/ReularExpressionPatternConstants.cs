493c22a842dc0551dc2e2ce6faf22cd9fbd0ee8b
= <=>
2024-02-17 17:03:07 +0530

    Added new constants class for regular expressions

---
+++ ClubMembership.API/ReularExpressionPatternConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembership.API
+{
+    public class ReularExpressionPatternConstants
+    {
+        //sample format: user@email.com
+        public static string EmailIdRegexPattern = "^([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$";
+
+        //8 to 15 alphanumeric special characters
+        public static string PasswordRegexPattern = "^([a-zA-Z0-9@*#]{8,15})$";
+    }
+}
