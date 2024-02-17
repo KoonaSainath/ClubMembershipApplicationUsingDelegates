4d4ea6e973af9c38d9ba2a1762dcb0ecd74c76de
= <=>
2024-02-17 16:56:10 +0530

    NullCheckTrim extension method created

---
+++ ClubMembership.API/ClubMembershipExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembership.API
+{
+    public static class ClubMembershipExtensionMethods
+    {
+        public static string NullCheckTrim(this string value)
+        {
+            string result = string.IsNullOrEmpty(value) ? string.Empty : value.Trim();
+            return result;
+        }
+    }
+}
