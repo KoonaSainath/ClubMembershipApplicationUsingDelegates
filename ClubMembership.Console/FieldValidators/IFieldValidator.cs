c34949494e09c554250625dac3c732164402258a
= <=>
2024-02-18 13:51:51 +0530

    Created common interface IFieldValidator for all validators to implement

---
+++ ClubMembership.Console/FieldValidators/IFieldValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembership.Console.FieldValidators
+{
+    public delegate bool DelValidateField(int fieldIndex, string fieldValue, string[] fieldArray, out string errorMessage);
+    public interface IFieldValidator
+    {
+        DelValidateField DelValidateField { get; }
+        string[] FieldArray { get; }
+    }
+}
