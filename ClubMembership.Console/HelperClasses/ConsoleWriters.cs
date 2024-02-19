3c22a019958d1ef340fce3bc334152b4474be2e8
= <=>
2024-02-19 21:21:16 +0530

    Created ConsoleWriters helper class to print some common outputs to console

---
+++ ClubMembership.Console/HelperClasses/ConsoleWriters.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembership.Console.HelperClasses
+{
+    public static class ConsoleWriters
+    {
+        private static string heading;
+        private static string register;
+        private static string login;
+
+        public static string Heading
+        {
+            get
+            {
+                heading = "Club membership registration";
+                return $"{heading}{Environment.NewLine}{new string('-',heading.Length)}";
+            }
+        }
+        public static string Registration
+        {
+            get
+            {
+                register = "Register";
+                return $"{register}{Environment.NewLine}{new string('-',register.Length)}";
+            }
+        }
+        public static string Login
+        {
+            get
+            {
+                login = "Login";
+                return $"{login}{Environment.NewLine}{new string('-',login.Length)}";
+            }
+        }
+        
+        public static void WriteHeading()
+        {
+            System.Console.WriteLine(Heading);
+        }
+        public static void WriteRegistration()
+        {
+            System.Console.WriteLine(Registration);
+        }
+        public static void WriteLogin()
+        {
+            System.Console.WriteLine(Login);
+        }
+    }
+}
