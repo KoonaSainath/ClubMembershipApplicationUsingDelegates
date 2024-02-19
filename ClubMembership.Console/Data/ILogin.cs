e4ec41a9695692b279c9ff8a7c5e31f9ba10b9cf
= <=>
2024-02-19 21:02:57 +0530

    Created ILogin interface with single method to login user

---
+++ ClubMembership.Console/Data/ILogin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembership.Console.Data
+{
+    public interface ILogin
+    {
+        bool IsLoginSuccessful(string emailId, string password);
+    }
+}
