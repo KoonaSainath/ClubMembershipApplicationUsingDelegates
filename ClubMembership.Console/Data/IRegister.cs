6aed79c3652c3215bfd0c2126ad973b8da11c044
= <=>
2024-02-19 20:41:03 +0530

    Created the interface IRegister with 2 methods as these 2 methods are data methods regards to registering of a user

---
+++ ClubMembership.Console/Data/IRegister.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembership.Console.Data
+{
+    public interface IRegister
+    {
+        void RegisterUser(string[] fields);
+        bool DoesEmailIdAlreadyExist(string emailId);
+    }
+}
