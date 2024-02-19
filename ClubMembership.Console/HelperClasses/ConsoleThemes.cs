3c909bee2270676ae252fada2a34bf8ca9ac8a39
= <=>
2024-02-19 21:25:57 +0530

    Created helper class ConsoleTheme with a method changing font colors of console using an enum value

---
+++ ClubMembership.Console/HelperClasses/ConsoleThemes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembership.Console.HelperClasses
+{
+    public enum ConsoleTheme
+    {
+        Success,
+        Failure,
+        Default
+    }
+    public static class ConsoleThemes
+    {
+        public static void SetTheme(ConsoleTheme theme)
+        {
+            switch (theme)
+            {
+                case ConsoleTheme.Success:
+                    System.Console.ForegroundColor = ConsoleColor.White;
+                    System.Console.BackgroundColor = ConsoleColor.Green;
+                    break;
+                case ConsoleTheme.Failure:
+                    System.Console.ForegroundColor = ConsoleColor.White;
+                    System.Console.BackgroundColor = ConsoleColor.Red;
+                    break;
+                default:
+                    System.Console.ResetColor();
+                    break;
+            }
+        }
+    }
+}
