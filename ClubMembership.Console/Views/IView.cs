986c4a4f721858d4b3091d7ab60cec75bcb62537
= <=>
2024-02-19 21:32:56 +0530

    Created the IView interface with IFieldValidator property and RunTheView method as these are 2 most common entities for a view

---
+++ ClubMembership.Console/Views/IView.cs
@@ -0,0 +1,15 @@
+﻿using ClubMembership.Console.FieldValidators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembership.Console.Views
+{
+    public interface IView
+    {
+        IFieldValidator FieldValidator { get; }
+        void RunTheView();
+    }
+}
