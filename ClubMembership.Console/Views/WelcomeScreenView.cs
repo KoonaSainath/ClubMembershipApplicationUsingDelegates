25e8c404f602006cc438ccf8639eb12f0d641e62
= <=>
2024-02-20 20:47:46 +0530

    Implemented WelcomeScreenView from IView

---
+++ ClubMembership.Console/Views/WelcomeScreenView.cs
@@ -0,0 +1,32 @@
+﻿using ClubMembership.Console.FieldValidators;
+using ClubMembership.Console.HelperClasses;
+using ClubMembership.Console.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembership.Console.Views
+{
+    public class WelcomeScreenView : IView
+    {
+        private User user;
+        public WelcomeScreenView(User user) 
+        {
+            this.user = user;
+        }
+        public IFieldValidator FieldValidator => null;
+
+        public void RunTheView()
+        {
+            ConsoleWriters.ClearConsole();
+            ConsoleWriters.WriteHeading();
+            string welcomeMessage = $"Welcome {user.FirstName} {user.LastName}! You are a proud member of our cycling club membership!";
+            ConsoleThemes.SetTheme(ConsoleTheme.Success);
+            ConsoleWriters.WriteToConsoleWithNewLine(welcomeMessage);
+            ConsoleThemes.SetTheme(ConsoleTheme.Default);
+            System.Console.ReadKey();
+        }
+    }
+}
